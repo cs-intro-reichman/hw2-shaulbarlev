94725e34c7dbcebb4dee6b1d9bdd18133f88c2c9
Shaul Bar-Lev <shaulbarlev1@gmail.com>
2025-11-05 00:59:43 +0200

    piiiiiii

--- CalcPi.java
+++ CalcPi.java
@@ -19,6 +19,6 @@ public class CalcPi {
 
 		}
 		System.out.println("pi according to Java: "+Math.PI);
-		System.out.println("pi, approximated: "+sum*4);
+		System.out.println("pi, approximated:     "+sum*4);
 	}
 }
